25c63a5c6adaf5f98e310b062855f710d9eaf564
aletrn <aletrn@users.noreply.huggingface.co>
2023-10-22 22:15:11 +0200

    [feat] reformat code, log context request id

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from src.utilities.utilities import setup_logging
+
+
+app_logger = setup_logging(debug=True)
--- src/utilities/type_hints.py
+++ src/utilities/type_hints.py
@@ -1,13 +1,6 @@
 """custom type hints"""
-from typing import List, Dict, Tuple
+from typing import List
 
-import numpy as np
-
-# ts_ddict1, ts_float64_1, ts_float64_2, ts_dict_str3, ts_dict_str2
 input_floatlist = List[float]
 input_floatlist2 = List[input_floatlist]
-ts_ddict1 = Dict[str, Dict[str, any], Dict, Dict, any]
-ts_dict_str2 = Dict[str, str]
-ts_dict_str3 = Dict[str, str, any]
-ts_float64_1 = Tuple[np.float64, np.float64, np.float64, np.float64, np.float64, np.float64]
-ts_float64_2 = Tuple[np.float64, np.float64, np.float64, np.float64, np.float64, np.float64, np.float64]
+
