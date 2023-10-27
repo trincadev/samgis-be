c6054f0c397f2e3c268d0221a7071675590998ca
aletrn <aletrn@users.noreply.huggingface.co>
2023-10-27 14:51:10 +0200

    [feat] switch to aws lambda powertools logger

--- src/app.py
+++ src/app.py
@@ -1,8 +1,22 @@
 import json
-import logging
+
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+logger = Logger()
 
 
-def lambda_handler(event, context):
-    logging.debug(f"event:{event}...")
-    logging.debug(f"context:{context}...")
-    return {"msg": f"ciao{json.dumps(event)}."}
+# @logger.inject_lambda_context
+def lambda_handler(event: dict, context: LambdaContext) -> dict[str]:
+    try:
+        logger.info("start")
+        logger.debug("debug...")
+        logger.info(f"event:{json.dumps(event)}...")
+        logger.info(f"context:{context}...")
+        logger.info("end")
+        if event["test"] > 100:
+            raise ValueError(f"value test {event['test']} too high!")
+        return {"test": event["test"]}
+    except Exception as e:
+        logger.error(f"exception:{e}.")
+        return {"msg": "internal server error"}
