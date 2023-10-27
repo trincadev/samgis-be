d413c45e717101f6c52054b6c813a1bc8cc15c32
ale <alenewmail-4@yahoo.it>
2023-10-27 18:29:49 +0200

    [fix] fix request, prepare run on aws lambda

--- src/app.py
+++ src/app.py
@@ -1,22 +1,61 @@
 import json
-
+import time
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from pydantic import BaseModel, ValidationError
+
+from src.utilities.type_hints import input_floatlist, input_floatlist2
+
 
 logger = Logger()
 
 
-# @logger.inject_lambda_context
+class BBoxWithPointInput(BaseModel):
+    bbox: input_floatlist
+    points: input_floatlist2
+
+
+def get_response(status: int, start_time: float, output: BaseModel = None) -> dict[str]:
+    """
+    Return a response for frontend clients.
+
+    Args:
+        status: status response
+        start_time: request start time (float)
+        output: dict we embed into our response
+
+    Returns:
+        dict: response
+
+    """
+    messages = {200: "ok", 422: "validation error", 500: "internal server error"}
+    body = {"duration_run": time.time() - start_time, "message": messages[status]}
+    if status == 200:
+        body = {"output": output.model_dump_json(), **body}
+    return {
+        "statusCode": status,
+        "headers": {'Content-type': 'application/json', 'Accept': 'application/json'},
+        "body": json.dumps(body)
+    }
+
+
 def lambda_handler(event: dict, context: LambdaContext) -> dict[str]:
+    logger.info(f"start with aws_request_id:{context.aws_request_id}.")
+    start_time = time.time()
     try:
-        logger.info("start")
-        logger.debug("debug...")
-        logger.info(f"event:{json.dumps(event)}...")
-        logger.info(f"context:{context}...")
-        logger.info("end")
-        if event["test"] > 100:
-            raise ValueError(f"value test {event['test']} too high!")
-        return {"test": event["test"]}
+        logger.debug(f"event:{json.dumps(event)}...")
+        logger.debug(f"context:{context}...")
+
+        try:
+            bbox_points = BBoxWithPointInput(bbox=event["bbox"], points=event["points"])
+            logger.info(f"validation ok, bbox_points:{bbox_points}...")
+            response = get_response(200, start_time, bbox_points)
+        except ValidationError as ve:
+            logger.error(f"validation error:{ve}.")
+            response = get_response(422, start_time)
     except Exception as e:
         logger.error(f"exception:{e}.")
-        return {"msg": "internal server error"}
+        response = get_response(500, start_time)
+
+    logger.info(f"response:{response}...")
+    return response
