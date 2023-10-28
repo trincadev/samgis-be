8ef9cc03619e1672bb19dcd3c8578dd05125536e
aletrn <aletrn@users.noreply.huggingface.co>
2023-10-29 00:20:37 +0200

    [fix] handle cases with b64encoded body, add test payload

--- src/utilities/utilities.py
+++ src/utilities/utilities.py
@@ -1,16 +1,19 @@
 """Various utilities (logger, time benchmark, args dump, numerical and stats info)"""
-import loguru
-
-from src.utilities.constants import ROOT
 
 
-def is_base64(s):
+def is_base64(sb):
     import base64
 
     try:
-        return base64.b64encode(base64.b64decode(s, validate=True)) == s
-    except Exception as e:
-        print("e:", e, "#")
+        if isinstance(sb, str):
+            # If there's any unicode here, an exception will be thrown and the function will return false
+            sb_bytes = bytes(sb, 'ascii')
+        elif isinstance(sb, bytes):
+            sb_bytes = sb
+        else:
+            raise ValueError("Argument must be string or bytes")
+        return base64.b64encode(base64.b64decode(sb_bytes, validate=True)) == sb_bytes
+    except ValueError:
         return False
 
 
