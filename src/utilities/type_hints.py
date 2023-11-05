d96977d60424deeb0967586870129473fec7f719
ale <alenewmail-4@yahoo.it>
2023-11-05 16:47:51 +0100

    [debug] add missing types

--- src/utilities/type_hints.py
+++ src/utilities/type_hints.py
@@ -1,6 +1,8 @@
 """custom type hints"""
-from typing import List
+from typing import List, Tuple
 
 input_floatlist = List[float]
 input_floatlist2 = List[input_floatlist]
+input_float_tuples = List[Tuple[float, float]]
 ts_dict_str2 = dict[str, str]
+ts_dict_str3 = dict[str, str, any]
