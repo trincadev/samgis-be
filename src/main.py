50f87032e49c7a41d53db7a4f1940cc224d46a9f
ale <alenewmail-4@yahoo.it>
2023-11-05 18:16:09 +0100

    [debug] add geopandas and rasterio to convert the FastSAM mask prediction into a geojson

--- src/main.py
+++ src/main.py
@@ -1,57 +1,14 @@
-from typing import Dict
+import rasterio
+from affine import loadsw
 
-from geojson_pydantic import Feature, Polygon, FeatureCollection
-from pydantic import BaseModel
-
-g1 = {
-    "type": "FeatureCollection",
-    "features": [{
-        "type": "Feature",
-        "geometry": {
-            "type": "Polygon",
-            "coordinates": [
-                [
-                    [13.1, 52.46385],
-                    [13.42786, 52.6],
-                    [13.2, 52.5],
-                    [13.38272, 52.4],
-                    [13.43, 52.46385],
-                    [13.1, 52.46385]
-                ]
-            ],
-        },
-        "properties": {
-            "name": "uno",
-        },
-    }, {
-        "type": "Feature",
-        "geometry": {
-            "type": "Polygon",
-            "coordinates": [
-                [
-                    [13.77, 52.8],
-                    [13.88, 52.77],
-                    [13.99, 52.66],
-                    [13.11, 52.55],
-                    [13.33, 52.44],
-                    [13.77, 52.8]
-                ]
-            ],
-        },
-        "properties": {
-            "name": "due",
-        },
-    }]
-}
-
-PolygonFeatureCollectionModel = FeatureCollection[Feature[Polygon, Dict]]
+from src import PROJECT_ROOT_FOLDER
 
 if __name__ == '__main__':
-    feat = PolygonFeatureCollectionModel(**g1)
-    print(feat)
-    print("feat")
-    """
-    
-    point:  {"lat":12.425847783029134,"lng":53.887939453125} 
-    map:ne:{"lat":17.895114303749143,"lng":58.27148437500001} sw:{"lat":0.6591651462894632,"lng":34.01367187500001}.
-    """
+    with open(PROJECT_ROOT_FOLDER / "tmp" / "japan_out_main.pgw") as pgw:
+        pgw_file = pgw.read()
+        a = loadsw(pgw_file)
+    with rasterio.open(PROJECT_ROOT_FOLDER / "tmp" / "japan_out_main.png", "r") as src:
+        src_transform = src.transform
+        print(a, src_transform)
+        print(a, src_transform)
+        print("a, src_tranform")
