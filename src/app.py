f43b1a3b677e04f96742e1999c9b6886ba9d10be
ale <alenewmail-4@yahoo.it>
2023-11-14 23:31:59 +0100

    [refactor] remove and transform unuseful logs to debug

--- src/app.py
+++ src/app.py
@@ -1,17 +1,15 @@
 import json
 import time
 from http import HTTPStatus
-from pathlib import Path
 from typing import Dict
 
 from aws_lambda_powertools.event_handler import content_types
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 from src import app_logger
-from src.io.coordinates_pixel_conversion import get_latlng_to_pixel_coordinates, get_point_latlng_to_pixel_coordinates
+from src.io.coordinates_pixel_conversion import get_latlng_to_pixel_coordinates
 from src.prediction_api.predictors import samexporter_predict
-from src.utilities.constants import CUSTOM_RESPONSE_MESSAGES, ROOT
-from src.utilities.serialize import serialize
+from src.utilities.constants import CUSTOM_RESPONSE_MESSAGES
 from src.utilities.utilities import base64_decode
 
 
@@ -59,32 +57,40 @@ def get_parsed_bbox_points(request_input: Dict) -> Dict:
     for prompt in request_input["prompt"]:
         app_logger.info(f"current prompt: {type(prompt)}, value:{prompt}.")
         data = prompt["data"]
-        if prompt["type"] == "rectangle":
-            app_logger.info(f"current data points: {type(data)}, value:{data}.")
-            data_ne = data["ne"]
-            app_logger.info(f"current data_ne point: {type(data_ne)}, value:{data_ne}.")
-            data_sw = data["sw"]
-            app_logger.info(f"current data_sw point: {type(data_sw)}, value:{data_sw}.")
-
-            diff_pixel_coords_origin_data_ne = get_latlng_to_pixel_coordinates(ne, sw, data_ne, zoom, "ne")
-            app_logger.info(f'current diff prompt ne: {type(data)}, {data} => {diff_pixel_coords_origin_data_ne}.')
-            diff_pixel_coords_origin_data_sw = get_latlng_to_pixel_coordinates(ne, sw, data_sw, zoom, "sw")
-            app_logger.info(f'current diff prompt sw: {type(data)}, {data} => {diff_pixel_coords_origin_data_sw}.')
-            prompt["data"] = [
-                diff_pixel_coords_origin_data_ne["x"], diff_pixel_coords_origin_data_ne["y"],
-                diff_pixel_coords_origin_data_sw["x"], diff_pixel_coords_origin_data_sw["y"]
-            ]
-        elif prompt["type"] == "point":
+        # if prompt["type"] == "rectangle":
+        #     app_logger.info(f"current data points: {type(data)}, value:{data}.")
+        #     data_ne = data["ne"]
+        #     app_logger.info(f"current data_ne point: {type(data_ne)}, value:{data_ne}.")
+        #     data_sw = data["sw"]
+        #     app_logger.info(f"current data_sw point: {type(data_sw)}, value:{data_sw}.")
+        #
+        #     diff_pixel_coords_origin_data_ne = get_latlng_to_pixel_coordinates(ne, sw, data_ne, zoom, "ne")
+        #     app_logger.info(f'current diff prompt ne: {type(data)}, {data} => {diff_pixel_coords_origin_data_ne}.')
+        #     diff_pixel_coords_origin_data_sw = get_latlng_to_pixel_coordinates(ne, sw, data_sw, zoom, "sw")
+        #     app_logger.info(f'current diff prompt sw: {type(data)}, {data} => {diff_pixel_coords_origin_data_sw}.')
+        #     prompt["data"] = [
+        #         diff_pixel_coords_origin_data_ne["x"], diff_pixel_coords_origin_data_ne["y"],
+        #         diff_pixel_coords_origin_data_sw["x"], diff_pixel_coords_origin_data_sw["y"]
+        #     ]
+        #     # rect_diffs_input = str(Path(ROOT) / "rect_diffs_input.json")
+        #     # with open(rect_diffs_input, "w") as jj_out3:
+        #     #     json.dump({
+        #     #         "prompt_data": serialize(prompt["data"]),
+        #     #         "diff_pixel_coords_origin_data_ne": serialize(diff_pixel_coords_origin_data_ne),
+        #     #         "diff_pixel_coords_origin_data_sw": serialize(diff_pixel_coords_origin_data_sw),
+        #     #     }, jj_out3)
+        #     # app_logger.info(f"written json:{rect_diffs_input}.")
+        if prompt["type"] == "point":
             current_point = get_latlng_to_pixel_coordinates(ne, sw, data, zoom, "point")
             app_logger.info(f"current prompt: {type(current_point)}, value:{current_point}.")
             new_prompt_data = [current_point['x'], current_point['y']]
             app_logger.info(f"new_prompt_data: {type(new_prompt_data)}, value:{new_prompt_data}.")
             prompt["data"] = new_prompt_data
         else:
-            raise ValueError("valid prompt types are only 'point' and 'rectangle'")
+            raise ValueError("valid prompt type is only 'point'")
 
     app_logger.info(f"bbox => {bbox}.")
-    app_logger.info(f'## request_input["prompt"] updated => {request_input["prompt"]}.')
+    app_logger.info(f'## request_input-prompt updated => {request_input["prompt"]}.')
 
     app_logger.info(f"unpacking elaborated {request_input}...")
     return {
@@ -125,9 +131,7 @@ def lambda_handler(event: dict, context: LambdaContext):
             app_logger.info(f"prompt_latlng:{prompt_latlng}.")
             body_request = get_parsed_bbox_points(body)
             app_logger.info(f"body_request=> {type(body_request)}, {body_request}.")
-            body_response = samexporter_predict(
-                body_request["bbox"], body_request["prompt"], body_request["zoom"], prompt_latlng
-            )
+            body_response = samexporter_predict(body_request["bbox"], body_request["prompt"], body_request["zoom"])
             app_logger.info(f"output body_response:{body_response}.")
             response = get_response(HTTPStatus.OK.value, start_time, context.aws_request_id, body_response)
         except Exception as ex2:
--- src/prediction_api/predictors.py
+++ src/prediction_api/predictors.py
@@ -1,18 +1,16 @@
 # Press the green button in the gutter to run the script.
-import json
+import tempfile
 from pathlib import Path
 from typing import List
 
 import numpy as np
 import rasterio
-from PIL import Image
 
 from src import app_logger, MODEL_FOLDER
 from src.io.tiles_to_tiff import convert
 from src.io.tms2geotiff import save_geotiff_gdal
 from src.prediction_api.sam_onnx import SegmentAnythingONNX
-from src.utilities.constants import MODEL_ENCODER_NAME, ZOOM, MODEL_DECODER_NAME, ROOT
-from src.utilities.serialize import serialize
+from src.utilities.constants import MODEL_ENCODER_NAME, MODEL_DECODER_NAME
 
 
 models_dict = {"fastsam": {"instance": None}}
@@ -51,128 +49,87 @@ def load_affine_transformation_from_matrix(matrix_source_coeffs: List):
         app_logger.error(f"exception:{e}, check https://github.com/rasterio/affine project for updates")
 
 
-def samexporter_predict(bbox, prompt: list[dict], zoom: float = ZOOM, model_name: str = "fastsam") -> dict:
+def samexporter_predict(bbox, prompt: list[dict], zoom: float, model_name: str = "fastsam") -> dict:
     try:
         from rasterio.features import shapes
         from geopandas import GeoDataFrame
 
         if models_dict[model_name]["instance"] is None:
-            app_logger.info(f"missing instance model {model_name}, instantiating it now")
+            app_logger.info(f"missing instance model {model_name}, instantiating it now!")
             model_instance = SegmentAnythingONNX(
                 encoder_model_path=MODEL_FOLDER / MODEL_ENCODER_NAME,
                 decoder_model_path=MODEL_FOLDER / MODEL_DECODER_NAME
             )
             models_dict[model_name]["instance"] = model_instance
-        app_logger.info(f"using a {model_name} instance model...")
+        app_logger.debug(f"using a {model_name} instance model...")
         models_instance = models_dict[model_name]["instance"]
 
-        img, matrix = convert(
-            bounding_box=bbox,
-            zoom=int(zoom)
-        )
-
-        pt0, pt1 = bbox
-        rio_output = f"/tmp/downloaded_rio_{pt0[0]}_{pt0[1]}_{pt1[0]}_{pt1[1]}.tif"
-        save_geotiff_gdal(img, rio_output, matrix)
-        app_logger.info(f"saved downloaded geotiff image to {rio_output}...")
-
-        np_img = np.array(img)
-        app_logger.info(f"## img type {type(np_img)}, prompt:{prompt}.")
-
-        app_logger.info(f"onnxruntime input shape/size (shape if PIL) {np_img.size},"
-                        f"start to initialize SamGeo instance:")
-        try:
-            app_logger.info(f"onnxruntime input shape (NUMPY) {np_img.shape}.")
-        except Exception as e_shape:
-            app_logger.error(f"e_shape:{e_shape}.")
-        app_logger.info(f"use {model_name} model, ENCODER model {MODEL_ENCODER_NAME} and"
-                        f" {MODEL_DECODER_NAME} from {MODEL_FOLDER}): model instantiated, creating embedding...")
-        embedding = models_instance.encode(np_img)
-        app_logger.info(f"embedding created, running predict_masks...")
-        prediction_masks = models_instance.predict_masks(embedding, prompt)
-        app_logger.info(f"predict_masks terminated...")
-        app_logger.info(f"predict_masks terminated, prediction masks shape:{prediction_masks.shape}, {prediction_masks.dtype}.")
-        pt0, pt1 = bbox
-        prediction_masks_output = f"/tmp/prediction_masks_{pt0[0]}_{pt0[1]}_{pt1[0]}_{pt1[1]}.npy"
-        np.save(
-            prediction_masks_output,
-            prediction_masks, allow_pickle=True, fix_imports=True
-        )
-        app_logger.info(f"saved prediction_masks:{prediction_masks_output}.")
-
-        mask = np.zeros((prediction_masks.shape[2], prediction_masks.shape[3]), dtype=np.uint8)
-        app_logger.info(f"output mask shape:{mask.shape}, {mask.dtype}.")
-        for n, m in enumerate(prediction_masks[0, :, :, :]):
-            app_logger.info(f"## {n} mask => m shape:{mask.shape}, {mask.dtype}.")
-            mask[m > 0.0] = 255
-        # prediction_masks0 = prediction_masks[0]
-        # app_logger.info(f"prediction_masks0 shape:{prediction_masks0.shape}.")
-        #
-        # try:
-        #     pmf = np.sum(prediction_masks0, axis=0).astype(np.uint8)
-        # except Exception as e_sum_pmf:
-        #     app_logger.error(f"e_sum_pmf:{e_sum_pmf}.")
-        #     pmf = prediction_masks0[0]
-        # app_logger.info(f"creating pil image from prediction mask with shape {pmf.shape}.")
-        # pil_pmf = Image.fromarray(pmf)
-        # pil_pmf_output = f"/tmp/pil_pmf_{pmf.shape[0]}_{pmf.shape[1]}.png"
-        # pil_pmf.save(pil_pmf_output)
-        # app_logger.info(f"saved pil_pmf:{pil_pmf_output}.")
-        #
-        # mask = np.zeros(pmf.shape, dtype=np.uint8)
-        # mask[pmf > 0] = 255
-
-        # cv2.imwrite(f"/tmp/cv2_mask_predicted_{mask.shape[0]}_{mask.shape[1]}_{mask.shape[2]}.png", mask)
-        pil_mask = Image.fromarray(mask)
-        pil_mask_predicted_output = f"/tmp/pil_mask_predicted_{mask.shape[0]}_{mask.shape[1]}.png"
-        pil_mask.save(pil_mask_predicted_output)
-        app_logger.info(f"saved pil_mask_predicted:{pil_mask_predicted_output}.")
-
-        mask_unique_values, mask_unique_values_count = serialize(np.unique(mask, return_counts=True))
-        app_logger.info(f"mask_unique_values:{mask_unique_values}.")
-        app_logger.info(f"mask_unique_values_count:{mask_unique_values_count}.")
-
-        app_logger.info(f"read geotiff:{rio_output}: create shapes_generator...")
-        # app_logger.info(f"image/geojson transform:{transform}: create shapes_generator...")
-        with rasterio.open(rio_output, "r", driver="GTiff") as rio_src:
-            band = rio_src.read()
-            try:
-                transform = load_affine_transformation_from_matrix(matrix)
-                app_logger.info(f"geotiff band:{band.shape}, type: {type(band)}, dtype: {band.dtype}.")
-                app_logger.info(f"geotiff band:{mask.shape}.")
-                app_logger.info(f"transform from matrix:{transform}.")
-                app_logger.info(f"rio_src crs:{rio_src.crs}.")
-                app_logger.info(f"rio_src transform:{rio_src.transform}.")
-            except Exception as e_shape_band:
-                app_logger.error(f"e_shape_band:{e_shape_band}.")
-                raise e_shape_band
-            # mask_band = band != 0
-            shapes_generator = ({
-                'properties': {'raster_val': v}, 'geometry': s}
-                for i, (s, v)
-                # in enumerate(shapes(mask, mask=(band != 0), transform=rio_src.transform))
-                # use mask=None to avoid using source
-                in enumerate(shapes(mask, mask=None, transform=rio_src.transform))
+        with tempfile.TemporaryDirectory() as input_tmp_dir:
+            img, matrix = convert(
+                bounding_box=bbox,
+                zoom=int(zoom)
             )
-            app_logger.info(f"created shapes_generator.")
-            shapes_list = list(shapes_generator)
-            app_logger.info(f"created {len(shapes_list)} polygons.")
-            gpd_polygonized_raster = GeoDataFrame.from_features(shapes_list, crs="EPSG:3857")
-            app_logger.info(f"created a GeoDataFrame...")
-            geojson = gpd_polygonized_raster.to_json(to_wgs84=True)
-            app_logger.info(f"created geojson...")
+            app_logger.debug(f"## img type {type(img)} with shape/size:{img.size}, matrix:{matrix}.")
 
-            output_geojson = str(Path(ROOT) / "geojson_output.json")
-            with open(output_geojson, "w") as jj_out:
-                app_logger.info(f"writing geojson file to {output_geojson}.")
-                json.dump(json.loads(geojson), jj_out)
-                app_logger.info(f"geojson file written to {output_geojson}.")
+            pt0, pt1 = bbox
+            rio_output = str(Path(input_tmp_dir) / f"downloaded_rio_{pt0[0]}_{pt0[1]}_{pt1[0]}_{pt1[1]}.tif")
+            app_logger.debug(f"saving downloaded geotiff image to {rio_output}...")
+            save_geotiff_gdal(img, rio_output, matrix)
+            app_logger.info(f"saved downloaded geotiff image to {rio_output}...")
 
-            return {
-                "geojson": geojson,
-                "n_shapes_geojson": len(shapes_list),
-                "n_predictions": len(prediction_masks),
-                # "n_pixels_predictions": zip_arrays(mask_unique_values, mask_unique_values_count),
-            }
+            np_img = np.array(img)
+            app_logger.info(f"## img type {type(np_img)}, prompt:{prompt}.")
+
+            app_logger.info(f"onnxruntime input shape/size (shape if PIL) {np_img.size}.")
+            try:
+                app_logger.info(f"onnxruntime input shape (NUMPY) {np_img.shape}.")
+            except Exception as e_shape:
+                app_logger.error(f"e_shape:{e_shape}.")
+            app_logger.info(f"instantiated model {model_name}, ENCODER {MODEL_ENCODER_NAME}, "
+                            f"DECODER {MODEL_DECODER_NAME} from {MODEL_FOLDER}: Creating embedding...")
+            embedding = models_instance.encode(np_img)
+            app_logger.info(f"embedding created, running predict_masks with prompt {prompt}...")
+            prediction_masks = models_instance.predict_masks(embedding, prompt)
+            app_logger.info(f"Created {len(prediction_masks)} prediction_masks,"
+                            f"shape:{prediction_masks.shape}, dtype:{prediction_masks.dtype}.")
+
+            mask = np.zeros((prediction_masks.shape[2], prediction_masks.shape[3]), dtype=np.uint8)
+            for n, m in enumerate(prediction_masks[0, :, :, :]):
+                app_logger.debug(f"## {n} mask => m shape:{mask.shape}, {mask.dtype}.")
+                mask[m > 0.0] = 255
+
+            app_logger.info(f"read downloaded geotiff:{rio_output} to create the shapes_generator...")
+
+            with rasterio.open(rio_output, "r", driver="GTiff") as rio_src:
+                band = rio_src.read()
+                try:
+                    app_logger.debug(f"geotiff band:{band.shape}, type: {type(band)}, dtype: {band.dtype}.")
+                    app_logger.debug(f"rio_src crs:{rio_src.crs}.")
+                    app_logger.debug(f"rio_src transform:{rio_src.transform}.")
+                except Exception as e_shape_band:
+                    app_logger.error(f"e_shape_band:{e_shape_band}.")
+                    raise e_shape_band
+                # mask_band = band != 0
+                shapes_generator = ({
+                    'properties': {'raster_val': v}, 'geometry': s}
+                    for i, (s, v)
+                    # in enumerate(shapes(mask, mask=(band != 0), transform=rio_src.transform))
+                    # use mask=None to avoid using source
+                    in enumerate(shapes(mask, mask=None, transform=rio_src.transform))
+                )
+                app_logger.info(f"created shapes_generator, transform it to a polygon list...")
+                shapes_list = list(shapes_generator)
+                app_logger.info(f"created {len(shapes_list)} polygons.")
+                gpd_polygonized_raster = GeoDataFrame.from_features(shapes_list, crs="EPSG:3857")
+                app_logger.info(f"created a GeoDataFrame, export to geojson...")
+                geojson = gpd_polygonized_raster.to_json(to_wgs84=True)
+                app_logger.info(f"created geojson...")
+
+                return {
+                    "geojson": geojson,
+                    "n_shapes_geojson": len(shapes_list),
+                    "n_predictions": len(prediction_masks),
+                    # "n_pixels_predictions": zip_arrays(mask_unique_values, mask_unique_values_count),
+                }
     except ImportError as e:
         app_logger.error(f"Error trying import module:{e}.")
