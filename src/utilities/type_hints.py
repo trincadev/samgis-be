dcc25fdd4e1caf393e15520200032fb7c26bbfe8
aletrn <aletrn@users.noreply.huggingface.co>
2023-11-19 00:58:00 +0100

    [feat] add first working use of pydantic input validation

--- src/utilities/type_hints.py
+++ src/utilities/type_hints.py
@@ -1,3 +1,13 @@
 """custom type hints"""
+from typing import TypedDict
+
+
 ts_dict_str2 = dict[str, str]
 ts_dict_str3 = dict[str, str, any]
+ts_ddict1 = dict[str, dict[str, any], dict, dict, any]
+# ts_list_float = list[float, float]
+
+
+class PixelCoordinate(TypedDict):
+    x: int
+    y: int
