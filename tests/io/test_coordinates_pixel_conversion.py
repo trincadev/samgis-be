238719bf7783856d68e084a6b97393284e304ba0
aletrn <aletrn@users.noreply.huggingface.co>
2023-11-20 22:40:31 +0100

    [test] add get_parsed_bbox_points test case, refactor test structure

--- tests/io/test_coordinates_pixel_conversion.py
+++ tests/io/test_coordinates_pixel_conversion.py
@@ -1,36 +1,13 @@
-import json
-from unittest import TestCase
-
-from src.io.coordinates_pixel_conversion import get_latlng2pixel_projection, get_point_latlng_to_pixel_coordinates, \
-    get_latlng_to_pixel_coordinates
-
-from tests import TEST_EVENTS_FOLDER
+from tests.io.test_utilities import fn_reading_json_inputs_outputs__test
 
 
-names_fn_dict = {
-    "get_latlng2pixel_projection": get_latlng2pixel_projection,
-    "get_point_latlng_to_pixel_coordinates": get_point_latlng_to_pixel_coordinates,
-    "get_latlng_to_pixel_coordinates": get_latlng_to_pixel_coordinates
-}
+def test_get_latlng2pixel_projection():
+    fn_reading_json_inputs_outputs__test(name_fn="get_latlng2pixel_projection")
 
 
-def test_fn_reading_json_inputs_outputs(name_fn):
-    fn = names_fn_dict[name_fn]
-
-    with open(TEST_EVENTS_FOLDER / f"{name_fn}.json") as tst_json:
-        inputs_outputs = json.load(tst_json)
-        for k, input_output in inputs_outputs.items():
-            print(f"k:{k}.")
-            output = fn(**input_output["input"])
-            assert output == input_output["output"]
+def test_get_point_latlng_to_pixel_coordinates():
+    fn_reading_json_inputs_outputs__test(name_fn="get_point_latlng_to_pixel_coordinates")
 
 
-class Test(TestCase):
-    def test_get_latlng2pixel_projection(self):
-        test_fn_reading_json_inputs_outputs("get_latlng2pixel_projection")
-
-    def test_get_point_latlng_to_pixel_coordinates(self):
-        test_fn_reading_json_inputs_outputs("get_point_latlng_to_pixel_coordinates")
-
-    def test_get_latlng_to_pixel_coordinates(self):
-        test_fn_reading_json_inputs_outputs("get_latlng_to_pixel_coordinates")
+def test_get_latlng_to_pixel_coordinates():
+    fn_reading_json_inputs_outputs__test(name_fn="get_latlng_to_pixel_coordinates")
