94729d449efa1c540b8b0de024003b5a83e46be6
aletrn <aletrn@users.noreply.huggingface.co>
2023-11-21 19:11:33 +0100

    [test] add get_raster_inference test case

--- tests/io/test_coordinates_pixel_conversion.py
+++ tests/io/test_coordinates_pixel_conversion.py
@@ -1,4 +1,4 @@
-from tests.io.test_utilities import fn_reading_json_inputs_outputs__test
+from tests.utilities_testing import fn_reading_json_inputs_outputs__test
 
 
 def test_get_latlng2pixel_projection():
---
+++ tests/prediction_api/test_predictors.py
@@ -0,0 +1,40 @@
+import json
+from unittest.mock import patch
+
+import numpy as np
+
+from src.prediction_api import sam_onnx
+from src.prediction_api.predictors import get_raster_inference
+from tests import TEST_EVENTS_FOLDER
+
+
+@patch.object(sam_onnx, "SegmentAnythingONNX")
+def test_get_raster_inference(
+    segment_anything_onnx_mocked
+):
+    name_fn = "get_raster_inference"
+
+    with open(TEST_EVENTS_FOLDER / f"{name_fn}.json") as tst_json:
+        inputs_outputs = json.load(tst_json)
+        for k, input_output in inputs_outputs.items():
+            model_mocked = segment_anything_onnx_mocked()
+
+            img = np.load(TEST_EVENTS_FOLDER / f"{name_fn}" / k / "img.npy")
+            inference_out = np.load(TEST_EVENTS_FOLDER / f"{name_fn}" / k / "inference_out.npy")
+            mask = np.load(TEST_EVENTS_FOLDER / f"{name_fn}" / k / "mask.npy")
+            prompt = input_output["input"]["prompt"]
+            model_name = input_output["input"]["model_name"]
+
+            model_mocked.embed.return_value = np.array(img)
+            model_mocked.embed.side_effect = None
+            model_mocked.predict_masks.return_value = inference_out
+            model_mocked.predict_masks.side_effect = None
+            print(f"k:{k}.")
+            output_mask, output_inference_out = get_raster_inference(
+                img=img,
+                prompt=prompt,
+                models_instance=model_mocked,
+                model_name=model_name
+            )
+            assert np.array_equal(output_mask, mask)
+            assert np.array_equal(output_inference_out, inference_out)
