9e27d227e89abef4e1874cfda00bbce53d803452
ale <alenewmail-4@yahoo.it>
2023-11-25 14:18:06 +0100

    [test] add test cases for get_vectorized_raster_as_geojson and SegmentAnythingONNX

--- tests/prediction_api/test_predictors.py
+++ tests/prediction_api/test_predictors.py
@@ -2,16 +2,15 @@ import json
 from unittest.mock import patch
 
 import numpy as np
+import rasterio
 
-from src.prediction_api import sam_onnx
-from src.prediction_api.predictors import get_raster_inference
+from src.prediction_api import predictors
+from src.prediction_api.predictors import get_raster_inference, samexporter_predict
 from tests import TEST_EVENTS_FOLDER
 
 
-@patch.object(sam_onnx, "SegmentAnythingONNX")
-def test_get_raster_inference(
-    segment_anything_onnx_mocked
-):
+@patch.object(predictors, "SegmentAnythingONNX")
+def test_get_raster_inference(segment_anything_onnx_mocked):
     name_fn = "samexporter_predict"
 
     with open(TEST_EVENTS_FOLDER / f"{name_fn}.json") as tst_json:
@@ -38,3 +37,32 @@ def test_get_raster_inference(
             )
             assert np.array_equal(output_mask, mask)
             assert len_inference_out == input_output["output"]["n_predictions"]
+
+
+@patch.object(predictors, "get_raster_inference")
+@patch.object(predictors, "SegmentAnythingONNX")
+@patch.object(predictors, "download_extent")
+@patch.object(predictors, "get_vectorized_raster_as_geojson")
+@patch.object(predictors, "get_affine_transform_from_gdal")
+def test_samexporter_predict(
+        get_affine_transform_from_gdal_mocked,
+        get_vectorized_raster_as_geojson_mocked,
+        download_extent_mocked,
+        segment_anything_onnx_mocked,
+        get_raster_inference_mocked
+):
+    """
+    model_instance = SegmentAnythingONNX()
+    img, matrix = download_extent(DEFAULT_TMS, pt0[0], pt0[1], pt1[0], pt1[1], zoom)
+    transform = get_affine_transform_from_gdal(matrix)
+    mask, n_predictions = get_raster_inference(img, prompt, models_instance, model_name)
+    get_vectorized_raster_as_geojson(mask, matrix)
+    """
+    aff = 1, 2, 3, 4, 5, 6
+    segment_anything_onnx_mocked.return_value = "SegmentAnythingONNX_instance"
+    download_extent_mocked.return_value = np.zeros((10, 10)), aff
+    get_affine_transform_from_gdal_mocked.return_value = rasterio.Affine.from_gdal(*aff)
+    get_raster_inference_mocked.return_value = np.ones((10, 10)), 1
+    get_vectorized_raster_as_geojson_mocked.return_value = {"geojson": "{}", "n_shapes_geojson": 2}
+    output = samexporter_predict(bbox=[[1, 2], [3, 4]], prompt=[{}], zoom=10, model_name="fastsam")
+    assert output == {"n_predictions": 1, "geojson": "{}", "n_shapes_geojson": 2}
