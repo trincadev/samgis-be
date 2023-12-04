c7f580c0d07b3ad160371a702ef859462c42e8fa
aletrn <aletrn@users.noreply.huggingface.co>
2023-12-04 18:18:49 +0100

    [test] add more "real" test cases for lambda_handler, 200/ok case

--- tests/test_app.py
+++ tests/test_app.py
@@ -140,68 +140,42 @@ class TestAppFailures(unittest.TestCase):
         print(f"types: response_200:{type(response_200)}, expected:{type(expected_response_200)}.")
         assert response_200 == expected_response_200
 
-    def test_lambda_handler_200_real(self):
+    def test_lambda_handler_200_real_single_multi_point(self):
         import shapely
 
         from src.app import lambda_handler
         from tests import LOCAL_URL_TILE, TEST_EVENTS_FOLDER
 
-        name_fn = "lambda_handler"
         invoke_id = "test_invoke_id"
 
-        with open(TEST_EVENTS_FOLDER / f"{name_fn}.json") as tst_json:
-            inputs_outputs = json.load(tst_json)
-            lambda_context = LambdaContext(
-                invoke_id=invoke_id,
-                client_context=None,
-                cognito_identity=None,
-                epoch_deadline_time_in_ms=time.time()
-            )
-            expected_response_dict = inputs_outputs["output"]
-            listen_port = 8000
+        for json_filename in ["lambda_handler_single_point.json", "lambda_handler_multipoint.json"]:
+            with open(TEST_EVENTS_FOLDER / json_filename) as tst_json:
+                inputs_outputs = json.load(tst_json)
+                lambda_context = LambdaContext(
+                    invoke_id=invoke_id,
+                    client_context=None,
+                    cognito_identity=None,
+                    epoch_deadline_time_in_ms=time.time()
+                )
+                expected_response_dict = inputs_outputs["output"]
+                listen_port = 8000
 
-            expected_response_body = json.loads(expected_response_dict["body"])
-            expected_output_geojson = shapely.from_geojson(expected_response_body["geojson"])
+                expected_response_body = json.loads(expected_response_dict["body"])
+                expected_output_geojson = shapely.from_geojson(expected_response_body["geojson"])
 
-            with LocalTilesHttpServer.http_server("localhost", listen_port, directory=TEST_EVENTS_FOLDER):
-                input_event = inputs_outputs["input"]
-                input_event_body = json.loads(input_event["body"])
-                input_event_body["url_tile"] = LOCAL_URL_TILE
-                input_event["body"] = json.dumps(input_event_body)
-                response = lambda_handler(event=input_event, context=lambda_context)
+                with LocalTilesHttpServer.http_server("localhost", listen_port, directory=TEST_EVENTS_FOLDER):
+                    input_event = inputs_outputs["input"]
+                    input_event_body = json.loads(input_event["body"])
+                    input_event_body["url_tile"] = LOCAL_URL_TILE
+                    input_event["body"] = json.dumps(input_event_body)
+                    response = lambda_handler(event=input_event, context=lambda_context)
 
-            response_dict = json.loads(response)
-            body_dict = json.loads(response_dict["body"])
-            assert body_dict["n_predictions"] == 1
-            assert body_dict["request_id"] == invoke_id
-            assert body_dict["message"] == "ok"
-            assert body_dict["n_shapes_geojson"] == expected_response_body["n_shapes_geojson"]
-            output_geojson = shapely.from_geojson(body_dict["geojson"])
-            assert shapely.equals_exact(output_geojson, expected_output_geojson, tolerance=0.000006)
-
-    def test_lambda_handler_200_real_multipoint(self):
-        from src.app import lambda_handler
-        from tests import LOCAL_URL_TILE, TEST_EVENTS_FOLDER
-
-        name_fn = "lambda_handler"
-        invoke_id = "test_invoke_id"
-
-        with open(TEST_EVENTS_FOLDER / f"{name_fn}.json") as tst_json:
-            inputs_outputs = json.load(tst_json)
-            lambda_context = LambdaContext(
-                invoke_id=invoke_id,
-                client_context=None,
-                cognito_identity=None,
-                epoch_deadline_time_in_ms=time.time()
-            )
-            listen_port = 8000
-
-            with LocalTilesHttpServer.http_server("localhost", listen_port, directory=TEST_EVENTS_FOLDER):
-                input_event = inputs_outputs["input"]
-                input_event_body = json.loads(input_event["body"])
-                input_event_body["url_tile"] = LOCAL_URL_TILE
-                input_event["body"] = json.dumps(input_event_body)
-                response = lambda_handler(event=input_event, context=lambda_context)
-
-            response_dict = json.loads(response)
-            assert response_dict["statusCode"] == 200
+                response_dict = json.loads(response)
+                assert response_dict["statusCode"] == 200
+                body_dict = json.loads(response_dict["body"])
+                assert body_dict["n_predictions"] == 1
+                assert body_dict["request_id"] == invoke_id
+                assert body_dict["message"] == "ok"
+                assert body_dict["n_shapes_geojson"] == expected_response_body["n_shapes_geojson"]
+                output_geojson = shapely.from_geojson(body_dict["geojson"])
+                assert shapely.equals_exact(expected_output_geojson, output_geojson, tolerance=0.000006)
