57f545cd4c232ba6933b89bfdd74153e32b655ed
aletrn <aletrn@users.noreply.huggingface.co>
2023-12-07 23:10:16 +0100

    [test] update test cases to handle input source as xyzservices.TileProvider

--- tests/io/test_tms2geotiff.py
+++ tests/io/test_tms2geotiff.py
@@ -11,8 +11,9 @@ input_bbox = [[39.036252959636606, 15.040283203125002], [38.302869955150044, 13.
 
 
 class TestTms2geotiff(unittest.TestCase):
-    def test_download_extent(self):
+    def test_download_extent_simple_source(self):
         from rasterio import Affine
+        from xyzservices import TileProvider
         from tests.local_tiles_http_server import LocalTilesHttpServer
 
         listen_port = 8000
@@ -26,13 +27,61 @@ class TestTms2geotiff(unittest.TestCase):
             s_lat = pt1[0]
             w_lng = pt1[1]
 
-            img, matrix = download_extent(w=w_lng, s=s_lat, e=e_lng, n=n_lat, zoom=zoom, source=LOCAL_URL_TILE)
+            source = TileProvider(name="local_tile_provider", url=LOCAL_URL_TILE, attribution="")
+            img, matrix = download_extent(w=w_lng, s=s_lat, e=e_lng, n=n_lat, zoom=zoom, source=source)
             app_logger.info(f"# DOWNLOAD ENDED, shape: {img.shape} #")
             np_img = np.ascontiguousarray(img)
             output_hash = hash_calculate(np_img)
             assert output_hash == b'UmbkwbPJpRT1XXcLnLUapUDP320w7YhS/AmT3H7u+b4='
             assert Affine.to_gdal(matrix) == (
                 1517657.1966021745, 152.8740565703525, 0.0, 4726942.266183584, 0.0, -152.87405657034955)
+
+    def test_download_extent_source_with_parameter(self):
+        from rasterio import Affine
+        from xyzservices import TileProvider
+        from tests.local_tiles_http_server import LocalTilesHttpServer
+
+        listen_port = 8000
+
+        with LocalTilesHttpServer.http_server("localhost", listen_port, directory=TEST_EVENTS_FOLDER):
+            pt0, pt1 = input_bbox
+            zoom = 10
+
+            n_lat = pt0[0]
+            e_lng = pt0[1]
+            s_lat = pt1[0]
+            w_lng = pt1[1]
+
+            local_url_tile2 = "http://localhost:8000/{parameter}/{z}/{x}/{y}.png"
+            source = TileProvider(
+                name="local_tile_provider", url=local_url_tile2, attribution="", parameter="lambda_handler")
+            img, matrix = download_extent(w=w_lng, s=s_lat, e=e_lng, n=n_lat, zoom=zoom, source=source)
+            app_logger.info(f"# DOWNLOAD ENDED, shape: {img.shape} #")
+            np_img = np.ascontiguousarray(img)
+            output_hash = hash_calculate(np_img)
+            assert output_hash == b'UmbkwbPJpRT1XXcLnLUapUDP320w7YhS/AmT3H7u+b4='
+            assert Affine.to_gdal(matrix) == (
+                1517657.1966021745, 152.8740565703525, 0.0, 4726942.266183584, 0.0, -152.87405657034955)
+
+    def test_download_extent_source_with_parameter_key_error(self):
+        from xyzservices import TileProvider
+
+        with self.assertRaises(KeyError):
+            try:
+                pt0, pt1 = input_bbox
+                zoom = 10
+
+                n_lat = pt0[0]
+                e_lng = pt0[1]
+                s_lat = pt1[0]
+                w_lng = pt1[1]
+
+                local_url_tile2 = "http://localhost:8000/{parameter}/{z}/{x}/{y}.png"
+                source = TileProvider(name="local_tile_provider", url=local_url_tile2, attribution="")
+                download_extent(w=w_lng, s=s_lat, e=e_lng, n=n_lat, zoom=zoom, source=source)
+            except KeyError as ke:
+                assert str(ke) == "'parameter'"
+                raise ke
 
     def test_download_extent_io_error1(self):
 
