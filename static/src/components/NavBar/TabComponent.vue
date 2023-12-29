5b52c2cfcf8a2661a0781e8c3051c4c11b663c1a
aletrn <aletrn@users.noreply.huggingface.co>
2023-12-30 00:24:41 +0100

    [feat] add navbar with blog, samgis presentation, samgis docs

---
+++ static/src/components/NavBar/TabComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>
+    <a :href="props.href"
+      class="bg-white border-2 no-underline pl-2 pr-2 p-1
+      landscape:border-gray-300 landscape:font-semibold landscape:text-lg"
+    >{{ props.description }}</a>
+  </h2>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  href: string,
+  description: string,
+}>()
+</script>
--- static/src/components/PageLayout.vue
+++ static/src/components/PageLayout.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="relative min-h-screen lg:flex">
-    <!-- Sidebar -->
+    <!-- Menubar -->
+    <NavBar class="hidden portrait:sd:hidden portrait:md:flex landscape:flex" style="z-index: 9999;"/>
+    <MobileNavBar class="flex portrait:sd:flex portrait:md:hidden landscape:hidden" style="z-index: 9999;"/>
 
-    <main id="content" class="flex-1 z-1 pb-12 ml-[3.3rem] lg:ml-0 mr-4 overflow-y-auto md:pl-1 lg:h-screen">
+    <main id="content" class="flex-1 z-1 lg:ml-0 mr-4 overflow-y-auto md:pl-1 lg:h-screen">
       <header class="hidden items-center justify-between h-10 ml-2 landscape:md:flex portrait:sd:flex portrait:md:h-12 bg-gray-200 border-b">
         <h2 class="hidden sd:text-sm ml-2 md:block md:text-2xl">{{  props.pageTitle }}</h2>
       </header>
@@ -21,6 +23,8 @@
 
 <script setup lang="ts">
 import Footer from "@/components/PageFooter.vue"
+import NavBar from '@/components/NavBar/NavBar.vue'
+import MobileNavBar from '@/components/NavBar/MobileNavBar.vue'
 
 const props = defineProps<{
   pageTitle: string
