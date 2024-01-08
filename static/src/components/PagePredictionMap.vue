893b5e275b4dc497dc27fa3f9a200ec09b30977d
aletrn <aletrn@users.noreply.huggingface.co>
2024-01-08 23:36:34 +0100

    [feat] add descriptions about exclude/include points

--- static/src/components/PagePredictionMap.vue
+++ static/src/components/PagePredictionMap.vue
@@ -59,6 +59,7 @@
         </div>
 
         <h1 id="id-ml-request-prompt">ML request prompt</h1>
+        <p>Exclude points: label 0, include points: label 1.</p>
         <div v-if="promptsArrayRef.filter(el => {return el.type === 'point'}).length > 0">
           <TableGenericComponent
             :header="['id', 'data', 'label']"
@@ -127,8 +128,8 @@ const driverObj = driver({
   steps: [
     { element: 'id-prediction-map-container', popover: { title: 'SamGIS', description: 'A quick tour about SamGIS functionality' } },
     { element: '#map', popover: { title: 'Webmap for ML prompt', description: 'Add here your machine learning prompt' } },
-    { element: '.leaflet-pm-icon-marker-include', popover: { title: '"Include" point prompt', description: 'add "include" points prompt for machine learning request' } },
-    { element: '.leaflet-pm-icon-marker-exclude', popover: { title: '"Exclude" point prompt', description: 'add "exclude" points prompt for machine learning request' } },
+    { element: '.leaflet-pm-icon-marker-include', popover: { title: '"Include" point prompt', description: 'add "include" points prompt (label 1) for machine learning request' } },
+    { element: '.leaflet-pm-icon-marker-exclude', popover: { title: '"Exclude" point prompt', description: 'add "exclude" points prompt (label 0) for machine learning request' } },
     { element: '.leaflet-pm-icon-rectangle', popover: { title: '"Include" rectangle prompt', description: 'add "include" rectangles prompt for machine learning request' } },
     { element: "#id-button-submit", popover: { title: 'ML submit button', description: 'Machine learning submit button' } },
     { element: '.leaflet-control-layers-toggle', popover: { title: 'Map provider selector', description: 'select a different map provider' } },
