d44501e6b885b8abe7f18bea7860058d5ce7aa5a
aletrn <aletrn@users.noreply.huggingface.co>
2024-04-17 12:36:19 +0200

    [doc] SamGIS version: handle case for package not installed; update README.md

--- samgis/__version__.py
+++ samgis/__version__.py
@@ -1,4 +1,8 @@
 import importlib.metadata
 
 
-__version__ = importlib.metadata.version(__package__ or __name__)
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except importlib.metadata.PackageNotFoundError or ImportError as e:
+    print(f"metadata::e: {type(e)}, {e}: package installed?")
+    __version__ = "1.3.0"
