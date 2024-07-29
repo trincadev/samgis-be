73b0d26fd4ba506699719d0cf46874afd13c1ff6
aletrn <aletrn@users.noreply.huggingface.co>
2024-07-29 23:43:13 +0200

    ci: add create_requirements.sh script for quick requirements.txt updates starting from installed packages (e.g. using poetry)

---
+++ scripts/create_requirements.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+SCRIPT=$(realpath "$0")
+SCRIPT_FOLDER=$(dirname "$SCRIPT")
+ROOT_FOLDER=${SCRIPT_FOLDER}/../
+
+rm ./tmp/requirements_tmp.txt || echo "./tmp/requirements_tmp.txt not found!"
+
+echo "start requirements.txt preparation: pip freeze..."
+pip freeze > ./tmp/freeze.txt
+echo "grep python dependencies into freeze.txt..."
+for x in $(cat ./requirements_no_versions.txt); do
+  echo "# $x #"
+  grep $x ./tmp/freeze.txt >> ./tmp/requirements_tmp.txt
+  echo "######"
+done
+
+echo "cat ${ROOT_FOLDER}/tmp/requirements_tmp.txt"
+cat ${ROOT_FOLDER}/tmp/requirements_tmp.txt
+echo -e "\n"
+
+[[ "$(echo -n 'Promote "${ROOT_FOLDER}/tmp/requirements_tmp.txt" as new requirements.txt? [y/N]> ' >&2; read; echo $REPLY)" == [Yy]* ]] \
+  && echo "copy requirements_tmp.txt to root project..." \
+  || exit 0
+
+cp ${ROOT_FOLDER}/tmp/requirements_tmp.txt ${ROOT_FOLDER}/requirements.txt
+
+echo "Fix any discrepancy within the new requirements.txt, bye!"
+
+exit 0
