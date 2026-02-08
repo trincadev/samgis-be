fc6d8052193049fc87a846c9573767f01b6b8964
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2026-02-08 23:58:25 +0100

    test: add playwright support and a first test case about the driver.js tour

---
+++ static/e2e/samgis-be.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test('test the driver.js tour on the localhost SamGIS-be page', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByRole('dialog', { name: 'SamGIS' })).toBeVisible();
+    await expect(page.locator('#driver-popover-description')).toContainText('A quick tour about SamGIS functionality');
+    await expect(page.getByRole('contentinfo')).toContainText('1 of 9');
+    await page.getByRole('button', { name: 'Next →' }).click();
+    await expect(page.getByRole('dialog', { name: 'Webmap for ML prompt' })).toBeVisible();
+
+    await page.getByRole('button', { name: 'Next →' }).click();
+    await expect(page.getByRole('dialog', { name: '"Include" point prompt' })).toBeVisible();
+
+    await page.getByRole('button', { name: 'Next →' }).click();
+    await expect(page.getByRole('dialog', { name: '"Exclude" point prompt' })).toBeVisible();
+
+    await page.getByRole('button', { name: 'Next →' }).click();
+    await expect(page.getByRole('dialog', { name: '"Include" rectangle prompt' })).toBeVisible();
+
+    await page.getByRole('button', { name: 'Next →' }).click();
+    await expect(page.getByRole('dialog', { name: 'ML submit button' })).toBeVisible();
+
+    await page.getByRole('button', { name: 'Next →' }).click();
+    await expect(page.getByRole('dialog', { name: 'Map provider selector' })).toBeVisible();
+
+    await page.getByRole('button', { name: 'Next →' }).click();
+    await expect(page.getByRole('dialog', { name: 'map info' })).toBeVisible();
+
+    await page.getByRole('button', { name: 'Next →' }).click();
+    await expect(page.locator('#driver-popover-description')).toContainText(
+      'Empty at beginning, this table will contain the machine learning prompt (points and rectangles) section'
+    );
+    await page.getByRole('button', { name: 'Done' }).click();
+});
